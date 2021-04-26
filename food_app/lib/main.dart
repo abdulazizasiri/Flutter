0bbde3b60e2e66bc1eabbbadebbea3338e4104b2
unknown <asiriaabdulaziz@gmail.com>
2021-04-26 21:20:29 +0300

    fixed all problems

--- food_app/lib/main.dart
+++ food_app/lib/main.dart
@@ -13,9 +13,10 @@ class MainStore extends StatelessWidget {
     return Scaffold(
 
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
+        // backgroundColor: Colors.transparent,
         // iconTheme: ,
       ),
+
        resizeToAvoidBottomInset: false,
 
 
@@ -26,23 +27,110 @@ class MainStore extends StatelessWidget {
             fit: BoxFit.fill,
           ),
         ),
+
         child: Container(
 
           width: double.infinity,
           height: MediaQuery.of(context).size.height,
           padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              // All items
-              Container(
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  RaisedButton(
+                      child: Text("الماكولات",style: TextStyle(color: Colors.white)),
+                      color: Colors.white,
+                      onPressed: null,
+                      shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))
+                  ),
+
+                  RaisedButton(
+                      child: Text("التراثية", style: TextStyle(color: Colors.white)),
+                      color: Colors.white,
+                      onPressed: null,
+                      shape: RoundedRectangleBorder( borderRadius: new BorderRadius.circular(30.0))
+                  ),
+
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+
+                  RaisedButton(
+                      child: Text("منتجات متنوعة", style: TextStyle(color: Colors.white)),
+                      color: Colors.white,
+                      onPressed: null,
+                      shape: RoundedRectangleBorder( borderRadius: new BorderRadius.circular(30.0))
+                  ),
+                  RaisedButton(
+                      child: Text("الضياقة",style: TextStyle(color: Colors.white)),
+                      color: Colors.white,
+                      onPressed: null,
+                      shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))
+                  ),
+
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                children: [
+
+                  Image.asset(
+                    "assets/foul.jpeg",
+                    height: 150,
+                    width: 150,
+                  ),
+                  Column(
+                    children: [
+                      Text("منتجات مميزة"),
+                      Image.asset(
+                        "assets/grapeLeaves.jpeg",
+                        height: 150,
+                        width: 150,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                children: [
+
+                  Image.asset(
+                    "assets/zeyed.jpeg",
+                    height: 150,
+                    width: 150,
+                  ),
+                  Column(
+                    children: [
+                      Text("الاكثر مبيعا"),
+                      Image.asset(
+                        "assets/eid.jpeg",
+                        height: 150,
+                        width: 150,
+                      ),
+
+                    ],
+
+                  )
+                ],
 
               ),
+
             ],
+
           ),
+
         ),
+
       ),
+
     );
 
 
